eca2cee60e2093d4b32c0db6684e5ae702762d1b
DanaImam <127887950+DanaImam@users.noreply.github.com>
2025-11-03 20:01:05 +0200

    Create counter_up_dwn.sv

---
+++ counter_up_dwn/counter_up_dwn.sv
@@ -0,0 +1,30 @@
+// Code your design here
+
+module counter_up_dwn( clk,reset,data,load,up_dwn,out);  
+  parameter SIZE= 4;
+  input clk;
+  input reset;
+  input up_dwn;
+  input [SIZE-1:0]data;
+  output [SIZE-1:0]out;
+  input load;
+  reg [SIZE-1:0]out;
+  
+  initial
+    out = 'b0;
+
+  always@(posedge clk)
+    if(reset) begin
+      out = 'b0;
+    end
+  else if(load) begin
+    out = data[SIZE-1:0];
+  end
+  else if (up_dwn) begin
+    out = out+ 1;
+  end
+  else begin
+    out = out- 1;
+  end
+
+endmodule
