ec3339b8119d96b0c42df37b388c6cb99f920c54
DanaImam <127887950+DanaImam@users.noreply.github.com>
2025-11-04 15:27:03 +0200

    Create div_by_3_seq_item.sv

---
+++ divisible-by-3-FSM-SystemVerilog/div_by_3_seq_item.sv
@@ -0,0 +1,16 @@
+`ifndef DIV_BY_3_SEQ_ITEM
+`define DIV_BY_3_SEQ_ITEM
+
+class div_by_3_seq_item;
+
+  //Input declared as random
+  rand logic bit_number;
+  rand logic res_n;
+  
+  //output signals
+  logic divl;
+
+
+endclass
+
+`endif
